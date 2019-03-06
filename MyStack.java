ed99867587c84d361401f1f8a63e58c16a7e6de4
Tochi <31775683+Tochi-kazi@users.noreply.github.com>
2019-03-06 14:47:01 -0500

    Create MyStack.java

---
+++ MyStack.java
@@ -0,0 +1,22 @@
+/**
+ * Name: Tochi Iroakazi
+ * Class: Unit 2-02
+ * Definition: sends commands to the Stacks program.
+ */
+
+import java.util.*;
+
+public class MyStack {
+
+	public static void main(String[] args) {
+		
+		Stacks stack = new Stacks();
+		
+		stack.push(14);
+		stack.push(12);
+		
+		stack.pop();
+
+	}
+
+}
