2120b8373f427235eb3eb6850a79e83cc1a7206b
Tochi <31775683+Tochi-kazi@users.noreply.github.com>
2019-03-06 14:46:22 -0500

    Create Stacks.java

---
+++ Stacks.java
@@ -0,0 +1,27 @@
+/**
+ * Name: Tochi Iroakazi
+ * Class: Unit 2-02
+ * Definition: Pops object from and Arraylist
+ */
+
+import java.util.*;
+public class Stacks {
+	
+	//Creating a stack that stores Integers
+	ArrayList<Integer> list = new ArrayList<Integer> (); 
+
+	public void push(int userinput) {
+		list.add(userinput);
+		System.out.println(list);
+
+	}
+	
+	public void pop() {
+		
+		if (list.size() == 0) {
+			throw new EmptyStackException();
+		}
+		 list.remove(list.size() - 1);
+		System.out.println(list);
+	}
+}
