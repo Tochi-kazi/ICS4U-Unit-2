c894faad4fa2bbe77b12d60c27e409a43a7baa9e
Tochi <31775683+Tochi-kazi@users.noreply.github.com>
2019-03-18 14:52:13 -0400

    Update Stacks.java

--- Stacks.java
+++ Stacks.java
@@ -7,17 +7,35 @@
 import java.util.*;
 public class Stacks {
 	
-	//Creating a stack that stores Integers
+	//Creating an arraylist that stores Integers
 	ArrayList<Integer> list = new ArrayList<Integer> (); 
 
 	public void push(int userinput) {
 		list.add(userinput);
-		System.out.println(list);
+		System.out.println("Whats in the list" + list);
 
 	}
 	
 	public void pop() {
-		 list.remove(list.size() - 1);
-		System.out.println(list);
+		//list.remove(list.size() - 1);
+		//System.out.println(list);
+		if (list.size() == 0) {
+			 throw new EmptyStackException();
+		}else {
+			list.remove(list.size() - 1);
+			System.out.println("After poping" + list);
+			
+		}
+		//list.remove(list.size() - 1);
+		//System.out.println(list);
+	}
+	
+	public void peek() {
+		System.out.println("The number at the top of the list is: " + list.get(list.size()-1));
+	}
+	
+	public void clear() {
+		list.clear();
+		System.out.println("Whats in the list after clearing: " + list);
 	}
 }
